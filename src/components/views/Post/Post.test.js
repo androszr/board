ecd85c9fd483777cde2ff1e69b2a250e5454d1ee
androszr <androszr@gmail.com>
2020-12-13 17:16:02 +0100

    update post test file

--- src/components/views/Post/Post.test.js
+++ src/components/views/Post/Post.test.js
@@ -2,9 +2,24 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { PostComponent } from './Post';
 
+const post = 
+  {
+    id: 1,
+    title: 'Card',
+    content: 'Content',
+    publicationDate: 'Publication date',
+    lastUpdate: 'Last update date',
+    email: 'Email',
+    status: 'Status',
+    photo: 'Photo',
+    price: 1,
+    phone: 123,
+    location: 'Poland',
+  };
+
 describe('Component Post', () => {
   it('should render without crashing', () => {
-    const component = shallow(<PostComponent />);
+    const component = shallow(<PostComponent post={post} />);
     expect(component).toBeTruthy();
   });
 });
